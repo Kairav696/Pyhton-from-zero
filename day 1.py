17af261c09759480707e99a38b5a172063b666c5
Kairav696 <kairavsinghm@gmail.com>
2026-01-29 22:18:12 +0530

    Personal Intro Program

---
+++ day 1.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+goal = input("Why do you want to learn Python? ")
+
+print("\n--- Your Python Intro ---")
+print("Hello,", name)
+print("You are", age, "years old.")
+print("Your goal is:", goal)
+print("Welcome to your coding journey 🚀")
